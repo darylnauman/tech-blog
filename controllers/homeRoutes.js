6c2a7c88afb4b648240b0bb558242281dbc87683
Daryl Nauman <darylnauman@gmail.com>
2021-06-28 20:41:08 -0400

    started preliminary controller info/files to be able to start server

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+// TO DO ROUTES
+
+
+
+
+module.exports = router;
