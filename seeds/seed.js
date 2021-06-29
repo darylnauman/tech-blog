a455f0e87c41e762d620c3e13f4846c1bd420c44
Daryl Nauman <darylnauman@gmail.com>
2021-06-28 20:16:04 -0400

    created file to seed data, starting with User data first; corrected issue in Comment & Post models

---
+++ seeds/seed.js
@@ -0,0 +1,20 @@
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+// const { Post, Comment } = require('../models');
+
+const userData = require('./userData.json');
+// const postData = require('./postData.json');
+// const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  })
+
+  process.exit(0);
+};
+
+seedDatabase();
