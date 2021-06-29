b3c8293d7b3168f6cd5b16f445a925c296dded3c
Daryl Nauman <darylnauman@gmail.com>
2021-06-29 15:46:45 -0400

    setup route for homepage to display all blogs; created initial homepage.handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,9 +2,30 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-// TO DO ROUTES
+router.get('/', async (req, res) => {
+  try {
+    // Get all posts
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name']
+        },
+      ],
+    });
 
-
+    // Serialize data
+    const posts = postData.map((post) => post.get({ plain: true}));
+    console.log(posts)
+    // pass serialized data and session flag into template
+    res.render('homepage',{page_title: 'Home Page', posts
+      
+      // logged_in: req.session.logged_in});
+    })
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
 
 
 module.exports = router;
