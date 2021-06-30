c070c9ed55cf8987a7cfde89a9fbd0dcc73b2933
Daryl Nauman <darylnauman@gmail.com>
2021-06-29 22:47:11 -0400

    some layout work on homepage, log-in feature live and working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -16,16 +16,26 @@ router.get('/', async (req, res) => {
 
     // Serialize data
     const posts = postData.map((post) => post.get({ plain: true}));
-    console.log(posts)
+    
     // pass serialized data and session flag into template
-    res.render('homepage',{page_title: 'Home Page', posts
-      
-      // logged_in: req.session.logged_in});
-    })
+    res.render('homepage', {
+      page_title: 'Home Page',
+      posts,
+      logged_in: req.session.logged_in
+    });
   } catch (err) {
     res.status(500).json(err)
   }
 });
 
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
 
 module.exports = router;
