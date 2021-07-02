4c04547ce77c8cfaa77830073179e6afaf9d667f
Daryl Nauman <darylnauman@gmail.com>
2021-07-01 20:21:10 -0400

    added sign-up feature - need to work on login page layout

--- public/js/login.js
+++ public/js/login.js
@@ -4,9 +4,6 @@ const loginFormHandler = async (event) => {
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  console.log(email);
-  console.log(password);
-
   if (email && password) {
     
     console.log('sending POST request to the API endpoint');
@@ -25,6 +22,32 @@ const loginFormHandler = async (event) => {
   }
 };
 
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (name && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
